9dccd085c6fc28a9abd87288c7043c26d86e7275
MeowcaTheoRange <tabletmeowcat@gmail.com>
2023-06-29 00:20:34 -0500

    Update ClockWidget

--- src/components/ClockWidget.tsx
+++ src/components/ClockWidget.tsx
@@ -1,7 +1,8 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "./ClockWidget.css";
 
 function ClockWidget() {
+  var [isHovering, setIsHovering] = useState(false);
   useEffect(() => {
     var canvas = document.getElementById("canvas");
     var time = document.getElementById("time");
@@ -14,20 +15,20 @@ function ClockWidget() {
     ctx.strokeStyle = "#FFDDBB";
     ctx.resetTransform();
     ctx.translate(radius, radius);
-    setInterval(drawTime, 1000);
-    drawTime();
+    requestAnimationFrame(drawTime);
 
     function drawTime() {
       var now = new Date(Date.now());
       var nowFormatted = now
         .toLocaleString(undefined, {
-          timeStyle: "short",
+          timeStyle: "medium",
           hour12: false,
           timeZone: "America/Chicago",
         })
         .split(":");
       var hour = +nowFormatted[0];
       var minute = +nowFormatted[1];
+      var second = +nowFormatted[2];
       // @ts-ignore
       ctx.clearRect(-radius, -radius, canvas.width, canvas.height);
 
@@ -36,16 +37,24 @@ function ClockWidget() {
       ctx.stroke();
 
       drawHand(
-        (hour * Math.PI) / 6 + (minute * Math.PI) / (6 * 60),
+        (hour * Math.PI) / 6 +
+          (minute * Math.PI) / (6 * 60) +
+          (second * Math.PI) / (6 * 60 * 60),
         radius * 0.6 - ctx.lineWidth
       );
-      drawHand((minute * Math.PI) / 30, radius * 0.8 - ctx.lineWidth);
+      drawHand(
+        (minute * Math.PI) / 30 + (second * Math.PI) / (30 * 60),
+        radius * 0.8 - ctx.lineWidth
+      );
+      if (isHovering)
+        drawHand((second * Math.PI) / 30, radius * 0.7 - ctx.lineWidth);
       // @ts-ignore
       time.innerHTML = now.toLocaleString(undefined, {
-        timeStyle: "short",
-        dateStyle: "short",
+        timeStyle: isHovering ? "medium" : "short",
+        dateStyle: isHovering ? "medium" : "short",
         timeZone: "America/Chicago",
       });
+      requestAnimationFrame(drawTime);
     }
 
     function drawHand(pos: number, length: number) {
@@ -59,11 +68,15 @@ function ClockWidget() {
     }
   });
   return (
-    <div className="ClockWidget">
+    <div
+      className="ClockWidget"
+      onMouseEnter={() => setIsHovering(true)}
+      onMouseLeave={() => setIsHovering(false)}
+    >
       <canvas id="canvas" width="64" height="64"></canvas>
       <div className="vertical">
         <span id="time">3:00 PM</span>
-        <span id="timezone">Central Time (GMT-6)</span>
+        <span id="timezone">Central Time (CT)</span>
       </div>
     </div>
   );
